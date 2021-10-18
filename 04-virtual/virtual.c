4e0642aa11aa25d7a261e04027953af80cb78b5e
Neal Fennimore <hi@neal.codes>
2021-10-18 14:16:14 -0400

    Update process pages to working

--- 04-virtual/virtual.c
+++ 04-virtual/virtual.c
@@ -133,17 +133,6 @@ int is_cached(struct PTE page_table[TABLEMAX], int page_number) {
     return page_table[page_number].is_valid;
 }
 
-int find_empty(struct PTE page_table[TABLEMAX], int *table_cnt){
-    for (int i = 0; i < *table_cnt; i++){
-        struct PTE current = page_table[i];
-        if (current.is_valid == 0){
-            return i;
-        }
-    }
-
-    return -1;
-}
-
 int get_usable_frame_number(int frame_pool[POOLMAX], int *frame_cnt){
     if (*frame_cnt == 0){
         return -1;
@@ -300,9 +289,84 @@ int process_page_access_fifo(struct PTE page_table[TABLEMAX], int *table_cnt, in
 // int count_page_faults_fifo(struct PTE page_table[TABLEMAX], int table_cnt, int refrence_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt);
 int count_page_faults_fifo(struct PTE page_table[TABLEMAX], int table_cnt, int refrence_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) { return -1; };
 
-// Please refer to Section 3.4 of the Modern Operating Systems book for a detailed discussion of the First-In First-Out algorithm.
 
-int process_page_access_lru(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp) { return -1; };
+int process_page_access_lru(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp){
+    // First the function checks if the page being referenced is already in memory (i.e., the page-table entry has the valid bit true). If so, it returns the frame number, after modifying the last_access_timestamp and the reference_count fields of the page-table entry.
+    if (is_cached(page_table, page_number))
+    {
+        struct PTE current = page_table[page_number];
+        current.last_access_timestamp = current_timestamp;
+        current.reference_count += 1;
+        page_table[page_number] = current;
+        return current.frame_number;
+    }
+
+    // If the page being referenced is not in memory, the function checks if there are any free frames (i.e., the process frame pool is not empty). If so, a frame is removed from the process frame pool and the frame number is inserted into the page-table entry corresponding to the logical page number. In addition, the other fields of the page-table entry are set appropriately. The function returns the frame number associated with the page-table entry.
+    if (*frame_cnt > 0)
+    {
+        int frame_number = get_usable_frame_number(frame_pool, frame_cnt);
+        set_page(page_table, page_number, frame_number, current_timestamp, 1);
+        print_page_tables(page_table, table_cnt);
+        return frame_number;
+    }
+
+    // If the page being referenced is not in memory and there are no free frames for the process, a page needs to be replaced. The function selects among all the pages of the process that are currently in memory (i.e., they have valid bits as true) the page that has the smallest last_access_timestamp. It marks that page-table entry as invalifd, along with setting the frame_number, arrival_timestamp, last_access_timestamp and reference_count to -1. It then sets the frame_number of the page-table entry of the newly-referenced page to the newly freed frame. It also sets the arrival_timestamp, the last_access_timestamp and the reference_count fields of the page-table entry appropriately. Finally, the function returns this frame number. 
+    int earliest_timestamp = INT_MAX;
+    int earliest_idx = 0;
+    for (int i = 0; i < *table_cnt; i++)
+    {
+        struct PTE current = page_table[i];
+        if (current.is_valid && current.last_access_timestamp < earliest_timestamp)
+        {
+            earliest_timestamp = current.last_access_timestamp;
+            earliest_idx = i;
+        }
+    }
+
+    struct PTE current = swap_page(page_table, page_number, earliest_idx, current_timestamp, 1);
+    print_page_tables(page_table, table_cnt);
+    print_page_table(current);
+    return current.frame_number;
+};
 int count_page_faults_lru(struct PTE page_table[TABLEMAX], int table_cnt, int refrence_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) { return -1; };
-int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp) { return -1; };
+int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp) {
+    // First the function checks if the page being referenced is already in memory (i.e., the page-table entry has the valid bit true). If so, it returns the frame number, after modifying the last_access_timestamp and the reference_count fields of the page-table entry.
+    if (is_cached(page_table, page_number))
+    {
+        struct PTE current = page_table[page_number];
+        current.last_access_timestamp = current_timestamp;
+        current.reference_count += 1;
+        page_table[page_number] = current;
+        return current.frame_number;
+    }
+
+    // If the page being referenced is not in memory, the function checks if there are any free frames (i.e., the process frame pool is not empty). If so, a frame is removed from the process frame pool and the frame number is inserted into the page-table entry corresponding to the logical page number. In addition, the other fields of the page-table entry are set appropriately. The function returns the frame number associated with the page-table entry.
+    if (*frame_cnt > 0)
+    {
+        int frame_number = get_usable_frame_number(frame_pool, frame_cnt);
+        set_page(page_table, page_number, frame_number, current_timestamp, 1);
+        print_page_tables(page_table, table_cnt);
+        return frame_number;
+    }
+
+    // If the page being referenced is not in memory and there are no free frames for the process, a page needs to be replaced. The function selects among all the pages of the process that are currently in memory (i.e., they have valid bits as true) the page that has the smallest reference_count. If multiple pages have the smallest reference_count, the one with the smallest arrival_timestamp among these is selected. After selecting the page for replacement, the function marks that page-table entry as invalid, and sets the frame_number, arrival_timestamp, last_access_timestamp and reference_count to -1. It then sets the frame_number of the page-table entry of the newly-referenced page to the newly freed frame. It also sets the arrival_timestamp, the last_access_timestamp and the reference_count fields of the page-table entry appropriately. Finally, the function returns this frame number. 
+    int earliest_timestamp = INT_MAX;
+    int least_ref_cnt = INT_MAX;
+    int earliest_idx = 0;
+    for (int i = 0; i < *table_cnt; i++)
+    {
+        struct PTE current = page_table[i];
+        if (current.is_valid && current.arrival_timestamp < earliest_timestamp && current.reference_count <= least_ref_cnt)
+        {
+            earliest_timestamp = current.arrival_timestamp;
+            least_ref_cnt = current.reference_count;
+            earliest_idx = i;
+        }
+    }
+
+    struct PTE current = swap_page(page_table, page_number, earliest_idx, current_timestamp, 1);
+    print_page_tables(page_table, table_cnt);
+    print_page_table(current);
+    return current.frame_number;
+};
 int count_page_faults_lfu(struct PTE page_table[TABLEMAX], int table_cnt, int refrence_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) { return -1; };
